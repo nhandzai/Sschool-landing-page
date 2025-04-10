0b2c172367b969cb8d5db7456e04694562ed0d92
nhandzai <ducnhan1522004@gmail.com>
2025-04-11 01:09:03 +0700

    fix scroll to top - dont use useRef

--- src/component/ScrollToTop.js
+++ src/component/ScrollToTop.js
@@ -1,13 +1,12 @@
 "use client"
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaArrowUp } from "react-icons/fa";
 
 export default function ScrollToTop() {
-  const topRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
 
   const handleScrollToTop = () => {
-    topRef.current.scrollIntoView({ behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   useEffect(() => {
@@ -25,12 +24,10 @@ export default function ScrollToTop() {
 
   return (
     <>
-      <div ref={topRef}></div>
-
       {isVisible && (
         <div
           onClick={handleScrollToTop}
-          className="h-12 w-12 text-center bg-4 rounded-full fixed z-20 bottom-100 right-4  cursor-pointer hover:bg-3 transition-all duration-300 ease-in-out flex items-center justify-center"
+          className="h-12 w-12 text-center bg-4 rounded-full fixed z-20 bottom-100 right-4 cursor-pointer hover:bg-3 transition-all duration-300 ease-in-out flex items-center justify-center"
         >
           <FaArrowUp className="text-white" title="Scroll to top" />
         </div>
